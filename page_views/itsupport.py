759a06561d67c3d6852d9884766b21ce8e441391
Harish Dhanarajan <42219646+harishdhanarajan@users.noreply.github.com>
2024-04-16 12:41:20 +0530

    Update itsupport.py

--- page_views/itsupport.py
+++ page_views/itsupport.py
@@ -13,40 +13,42 @@ class ITSupport():
             self._content_placeholder = st.empty()
     def display_page(self):
         st.markdown("<p style = 'text-align:center;'> Please use the below search bar for your issues - Presented by BNYM. </p>", unsafe_allow_html = True)
-        user_query = st.text_input("", value = "")
-        links = pd.read_excel("links.xlsx")
-        top_n=5
-        threshold = 0.6
-        model = SentenceTransformer('bert-base-nli-mean-tokens')
-        embeddings = model.encode(links['Description'].tolist())
-        query_embedding = model.encode([user_query])[0]
-        similarities = cosine_similarity([query_embedding], embeddings)[0]
-        indices = similarities.argsort()[-top_n:][::-1]
-        relevant_results = []
-        
-        for idx, similarity in enumerate(similarities):
-            if similarity > threshold:
-                if any(keyword.lower() in links.iloc[idx]['Description'].lower() for keyword in user_query.split()):
-                    relevant_results.append(links.iloc[idx])
+        col1, col2 = st.beta_columns([3, 1])
+        with col1:
+            user_query = st.text_input("", value = "")
+            links = pd.read_excel("links.xlsx")
+            top_n=5
+            threshold = 0.6
+            model = SentenceTransformer('bert-base-nli-mean-tokens')
+            embeddings = model.encode(links['Description'].tolist())
+            query_embedding = model.encode([user_query])[0]
+            similarities = cosine_similarity([query_embedding], embeddings)[0]
+            indices = similarities.argsort()[-top_n:][::-1]
+            relevant_results = []
             
-     
-        if st.button("Search"):
-            results = pd.DataFrame(relevant_results)
-            if results.empty:
-                print('No Relevant Topics to Display')
-            else:
-                N_cards_per_row = 2
-                    
-                if user_query:
-                    for n_row, row in results.reset_index().iterrows():
-                        i = n_row%N_cards_per_row
-                        if i==0:
-                            st.write("---")
-                            cols= st.columns(N_cards_per_row, gap = "large")
-                            
-                        with cols[n_row%N_cards_per_row]:
-                            st.markdown("---")
-                            st.markdown(f"**{row['Description'].strip()}**")
-                            st.markdown(f"**{row['Related To'].strip()}**")
-                            st.markdown(f"**{row['Link']}**")
-                            
+            for idx, similarity in enumerate(similarities):
+                if similarity > threshold:
+                    if any(keyword.lower() in links.iloc[idx]['Description'].lower() for keyword in user_query.split()):
+                        relevant_results.append(links.iloc[idx])
+            
+        with col2:
+            if st.button("Search"):
+                results = pd.DataFrame(relevant_results)
+                if results.empty:
+                    print('No Relevant Topics to Display')
+                else:
+                    N_cards_per_row = 2
+                        
+                    if user_query:
+                        for n_row, row in results.reset_index().iterrows():
+                            i = n_row%N_cards_per_row
+                            if i==0:
+                                st.write("---")
+                                cols= st.columns(N_cards_per_row, gap = "large")
+                                
+                            with cols[n_row%N_cards_per_row]:
+                                st.markdown("---")
+                                st.markdown(f"**{row['Description'].strip()}**")
+                                st.markdown(f"**{row['Related To'].strip()}**")
+                                st.markdown(f"**{row['Link']}**")
+                                
