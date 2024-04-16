e0699584f795e5867e886385616f57a735af41ef
Harish Dhanarajan <42219646+harishdhanarajan@users.noreply.github.com>
2024-04-16 12:47:05 +0530

    Update itsupport.py

--- page_views/itsupport.py
+++ page_views/itsupport.py
@@ -14,7 +14,7 @@ class ITSupport():
     def display_page(self):
         st.markdown("<p style = 'text-align:center;'> Please use the below search bar for your issues - Presented by BNYM. </p>", unsafe_allow_html = True)
         col1,col2,col3 = st.columns([1, 3, 1])
-        with col1:
+        with col2:
             user_query = st.text_input("", value = "")
             links = pd.read_excel("links.xlsx")
             top_n=5
@@ -31,24 +31,22 @@ class ITSupport():
                     if any(keyword.lower() in links.iloc[idx]['Description'].lower() for keyword in user_query.split()):
                         relevant_results.append(links.iloc[idx])
             
-        with col2:
-            if st.button("Search"):
-                results = pd.DataFrame(relevant_results)
-                if results.empty:
-                    print('No Relevant Topics to Display')
-                else:
-                    N_cards_per_row = 2
+        if results.empty:
+            st.write("---")
+            st.write('No Relevant Topics to Display - Please search again!')
+        else:
+            N_cards_per_row = 2
+            if user_query:
+                for n_row, row in results.reset_index().iterrows():
+                    i = n_row%N_cards_per_row
+                    if i==0:
+                        st.write("---")
+                        cols= st.columns(N_cards_per_row, gap = "large")
                         
-                    if user_query:
-                        for n_row, row in results.reset_index().iterrows():
-                            i = n_row%N_cards_per_row
-                            if i==0:
-                                st.write("---")
-                                cols= st.columns(N_cards_per_row, gap = "large")
+                        with cols[n_row%N_cards_per_row]:
+                            st.markdown("---")
+                            st.markdown(f"**{row['Description'].strip()}**")
+                            st.markdown(f"**{row['Related To'].strip()}**")
+                            st.markdown(f"**{row['Link']}**")
                                 
-                            with cols[n_row%N_cards_per_row]:
-                                st.markdown("---")
-                                st.markdown(f"**{row['Description'].strip()}**")
-                                st.markdown(f"**{row['Related To'].strip()}**")
-                                st.markdown(f"**{row['Link']}**")
-                                
+
